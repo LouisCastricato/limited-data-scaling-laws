73e783034e327efdb52a0721174ed35544ffae1c
LouisCastricato <louis_castricto@brown.edu>
2022-10-30 23:45:52 +0000

    Need to resolve critic models, done for the night

--- ppo_main.py
+++ ppo_main.py
@@ -17,32 +17,32 @@ if __name__ == "__main__":
     def match_function(prior, player1, player2):
         return critic_model.match_function(prior, player1, player2)
 
-    # test the elo_schedule
-    elo_out = elo_schedule("Avatar the last airbender - Complete Series DVD", 
-    ["This is quite possibly the best show ever. I'm happy with my purchase.", "I really didn't like this show, there were many issues. I want to return this.",\
-     "Possibly the worst show I've ever seen.", "Best purchase ever! I love this show.", "I loved this show as a child, I think it still holds up."], 
-    match_function, step_factor=2, tournament_size=3, samples=20)
-    print(elo_out)
-
-    def reward_fn(samples : List[str], **kwargs) -> List[float]:
+    def reward_fn(samples : List[str]) -> List[float]:
         """
         samples: list of strings for the samples
         prior: string for the prior
         Returns a list of rewards for each sample.
         """
-        # get the match function
-        rewards = elo_schedule(kwargs['prior'], samples, match_function)[1]
+        # get the match function, No prior
+        rewards = elo_schedule(None, samples, match_function)[1]
 
-        # normalize the scores. highest elo is 4000, lowest is 1000
-        rewards = list(map(lambda x: x/4000.0, scores))
+        # normalize the scores using std and mean
+        rewards = (rewards - torch.mean(rewards)) / torch.std(rewards)
 
         # return the rewards
         return rewards
 
-    #model = trlx.train(
-    #    "finetuned_student_model/",
-    #    reward_fn=reward_fn,
-    #    prompts=["This is a prior"],
-    #    eval_prompts=["This is a different prior"]
-    #)
+    imdb = load_dataset("imdb", split="train+test")
+    prompts = [" ".join(review.split()[:4]) for review in imdb["text"]]
+    # 95% for train
+    train_prompts = prompts[:int(len(prompts)*0.95)]
+    # 5% for test
+    test_prompts = prompts[int(len(prompts)*0.95):]
+
+    model = trlx.train(
+        "gpt2-large",
+        reward_fn=reward_fn,
+        prompts=train_prompts,
+        eval_prompts=test_prompts
+    )
     
